6bae29c1d1a56f0bae546767e75881b73d00a4a9
Raphael Narasaki <narasakir@gmail.com>
2018-06-23 00:51:36 -0300

    Setting up the Mongoose ODM

---
+++ models/user.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String, 
+    required: true, 
+    trim: true
+  },
+  password: { 
+    type: String, 
+    required: true, 
+    trim: true 
+  }
+});
+
+export default mongoose.model('User', UserSchema);
